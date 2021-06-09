bcfc8a127917d060fa1904ab82c0c260628d333f
yusei <yu2000212@icloud.com>
2021-06-09 21:58:27 +0900

    [add] ログイン認証テストを追加
    メールアドレスとパスワードでログインしてから、/listにアクセスできるか確認する為にログイン認証を追加しました。

--- backend/tests/Feature/StockTest.php
+++ backend/tests/Feature/StockTest.php
@@ -18,16 +18,29 @@ class StockTest extends TestCase
 
     use RefreshDatabase;
 
-    public function test_login()
+    public function test_login_screen_can_be_rendered()
     {
         $response = $this->get('/login');
         $response->assertStatus(200);
     }
 
-    public function test_index()
+    public function test_list_screen_can_be_rendered()
     {
         $response = $this->get('/list');
         $response->assertStatus(302);
+    }
+
+    public function test_Can_Login(): void
+    {
+        $user = User::factory(User::class)->create([
+            'password' => bcrypt('password'),
+        ]);
+        $response = $this->from('/login')->post('/login', [
+            'email' => $user->email,
+            'password' => 'password',
+        ]);
+        $response->assertRedirect('/list');
+        $this->assertAuthenticatedAs($user);
     }
 
     public function test_add_screen_can_be_rendered()
