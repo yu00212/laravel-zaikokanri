dc318c503672b35294479a12d3da9cc8a6295d8e
yusei <yu2000212@icloud.com>
2021-07-22 19:32:56 +0900

    [fix] ルート先の変更
    管理者用のアカウント作成画面でルート先を管理者専用になるよう変更しました。

--- backend/resources/views/admin/auth/register.blade.php
+++ backend/resources/views/admin/auth/register.blade.php
@@ -10,7 +10,7 @@
 
         <x-jet-validation-errors class="mb-4" />
 
-        <form method="POST" action="{{ route('register') }}">
+        <form method="POST" action="{{ route('admin.register') }}">
             @csrf
 
             <div>
@@ -51,7 +51,7 @@
             @endif
 
             <div class="flex items-center justify-end mt-4">
-                <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('login') }}">
+                <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('admin.login') }}">
                     {{ __('アカウントを持っている方はこちら') }}
                 </a>
 
