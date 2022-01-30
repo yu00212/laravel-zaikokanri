2fd3766563f85b1412b2bed194b4d9ec0bc0f31c
yu00212 <yu2000212@icloud.com>
2022-01-30 20:57:28 +0900

    [fix] テストコード修正
    アカウント作成画面で、アカウント作成後のリダイレクト先が正しいかのテストを修正しました。

--- backend/tests/Feature/RegistrationTest.php
+++ backend/tests/Feature/RegistrationTest.php
@@ -24,7 +24,6 @@ class RegistrationTest extends TestCase
             'name' => 'Test User',
             'email' => 'test@example.com',
             'password' => 'password',
-            'password_confirmation' => 'password',
         ]);
 
         $this->assertAuthenticatedAs($response);
