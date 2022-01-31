defb300a6f0c76c117bc1f7ac4d8b3c16e371057
yu00212 <yu2000212@icloud.com>
2022-01-31 17:39:07 +0900

    [fix] forceScheme追記
    EC2のALBでドメインをSSL化したところ、ログイン画面でPOST時に「送信情報が保護されない」と警告が発生するようになったため、設定を追記しました。

--- backend/app/Providers/AppServiceProvider.php
+++ backend/app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Routing\UrlGenerator;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -20,10 +21,11 @@ class AppServiceProvider extends ServiceProvider
     /**
      * Bootstrap any application services.
      *
+     * @param UrlGenerator $url
      * @return void
      */
-    public function boot()
+    public function boot(UrlGenerator $url)
     {
-        Schema::defaultStringLength(191); //デフォルトのインデックス用文字列長を191に変更
+        $url->forceScheme('https');
     }
 }
