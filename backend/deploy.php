d8c227fc49a023be1e273ff85e2a52e08fc1b43a
yu00212 <yu2000212@icloud.com>
2022-02-03 00:19:13 +0900

    [add] 自動デプロイの設定追加
    ワークフローでデプロイコマンドを実行するための設定ファイルを追加しました。

---
+++ backend/deploy.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace Deployer;
+
+require 'recipe/laravel.php';
+
+// Project name
+set('application', 'zaikokanri');
+
+// Project repository
+set('repository', 'https://github.com/yu00212/laravel-zaikokanri.git');
+
+// [Optional] Allocate tty for git clone. Default value is false.
+set('git_tty', true);
+
+// Shared files/dirs between deploys
+add('shared_files', []);
+add('shared_dirs', []);
+
+// Writable dirs by web server
+add('writable_dirs', []);
+
+
+// Hosts
+
+host('github.com')
+    ->port(22)
+    ->user('root')
+    ->stage('test')
+    ->set('branch', 'main')
+    ->set('deploy_path', '/var/www/laravel-zaikokanri/backend');
+
+// [Optional] if deploy fails automatically unlock.
+after('deploy:failed', 'deploy:unlock');
+
+// Migrate database before symlink new release.
+
+before('deploy:symlink', 'artisan:migrate');
+
+// Tasks
+
+task('deploy', function () {
+    // 本番への反映は確認を挟む
+    if (input()->hasArgument('stage') && (input()->getArgument('stage') === 'production')) {
+        if (!askConfirmation('productionに反映して問題ありませんか？', true)) {
+            writeln('deploy was stopped');
+            return;
+        }
+    }
+    invoke('deploy:laravel');
+});
+
+desc('shared/.envを.env.{stage}で上書き');
+task('overwrite-env', function () {
+    $stage = get('stage');
+    $src = ".env.${stage}";
+    $deployPath = get('deploy_path');
+    $sharedPath = "${deployPath}/shared";
+    run("cp -f {{release_path}}/${src} ${sharedPath}/.env");
+});
+
+/**
+ * Main task
+ */
+desc('Deploy your project');
+task('deploy:laravel', [
+    'deploy:info',
+    'deploy:prepare',
+    'deploy:lock',
+    'deploy:release',
+    'deploy:update_code',
+    'deploy:shared',
+    'deploy:vendors',
+    'deploy:writable',
+    'artisan:storage:link',
+    'artisan:view:clear',
+    'artisan:cache:clear',
+    'artisan:config:cache',
+    'artisan:optimize',
+    'deploy:symlink',
+    'deploy:unlock',
+    'cleanup',
+]);
