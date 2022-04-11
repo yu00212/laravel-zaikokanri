d4220944a14adfdc2fa15e944a9dfc31976ddb8c
yu00212 <yu2000212@icloud.com>
2022-04-12 01:17:45 +0900

    [update] アカウント作成画面に情報取得理由の説明を追加
    UX向上において情報取得に対するユーザーの不安を解消するため、情報取得理由の説明文を追加しました。

--- backend/resources/views/auth/register.blade.php
+++ backend/resources/views/auth/register.blade.php
@@ -6,22 +6,30 @@
 
         <x-jet-validation-errors class="mb-4" />
 
+        <h2 class="flex justify-center font-semibold text-xl text-gray-800 leading-tight mb-4">
+            あなたのことを教えてください
+        </h2>
+        <p class="flex justify-center text-sm">※設定情報はアプリ内のみで使用します。</p>
+
         <form method="POST" action="{{ route('register') }}">
             @csrf
 
             <div>
                 <x-jet-label for="name" value="名前" />
                 <x-jet-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
+                <p class="text-sm">※名前はアカウント名に使用します。</p>
             </div>
 
             <div class="mt-4">
                 <x-jet-label for="email" value="メールアドレス" />
                 <x-jet-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required />
+                <p class="text-sm">※メールアドレスはログイン時に使用します。</p>
             </div>
 
             <div class="mt-4">
                 <x-jet-label for="password" value="パスワード" />
                 <x-jet-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="new-password" />
+                <p class="text-sm">※パスワードはログイン時に使用します。</p>
             </div>
 
             <div class="mt-4">
