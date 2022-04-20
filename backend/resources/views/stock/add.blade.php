8af7f47320b25c2c5d7a4e41072039685c654bf1
yu00212 <yu2000212@icloud.com>
2022-04-21 00:39:11 +0900

    [update] 在庫登録での画像動的表示機能を追加
    在庫登録画面で画像を選択した場合にどんな画像を選択したかわからない状態だったので、選択した画像を動的表示させるよう修正しました。

--- backend/resources/views/stock/add.blade.php
+++ backend/resources/views/stock/add.blade.php
@@ -14,25 +14,41 @@
             <form action="/guest/list/addCheck" method="post" enctype="multipart/form-data" class="grid grid-cols-1 gap-6">
                 @csrf
                 @endcan
+
+                <div class="flex justify-center md:px-20 xl:px-20">
+                    <label class="block px-12 py-2 rounded-md bg-gray-300">
+                        <p class="text-center">画像を選択</p>
+                        <input id="img_upload" type="file" accept="image/*" name="image" value="{{old('image')}}" class="hidden -ml-12" onchange=" previewImage(this)">
+                        @error('image')
+                        <p class="w-44 text-red-500 text-sm">{{$message}}</p>
+                        @enderror
+                    </label>
+                </div>
+
                 <div class="container flex justify-center xl:-mt-2">
                     <div class="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-3">
                         <div></div>
                         <div class="flex justify-center">
-                            <label class="block px-12 py-2 rounded-md bg-gray-300">
-                                <p class="text-center">画像を選択</p>
-                                <input id="img_upload" type="file" accept="image/*" name="image" value="{{old('image')}}" class="hidden -ml-12 block rounded-md border-gray-300 shadow-sm focus:border-purple-300 focus:ring focus:ring-purple-200 focus:ring-opacity-30"></input>
-                                @error('image')
-                                <p class="w-44 text-red-500 text-sm">{{$message}}</p>
-                                @enderror
-                            </label>
-                        </div>
-
-                        <div class="flex justify-center">
                             <label class="block w-44">
-                                <!-- imgタグを設定(/image/Noimage.jpgのところは、publicディレクトリの配下の構成に応じて適当に変更してください。 -->
-                                <img id=" img_prv" src="https://zaikokanri.s3.ap-northeast-1.amazonaws.com/SQBzGcvgGvOftMYGWG85i2DBuXaONl6FbiW9uwoA.jpg" class="h-48 w-full">
-                                <label>
+                                <img id="preview" class="h-48 w-full" src="https://zaikokanri.s3.ap-northeast-1.amazonaws.com/SQBzGcvgGvOftMYGWG85i2DBuXaONl6FbiW9uwoA.jpg">
+                            </label>
+                            <script>
+                                function previewImage(obj) {
+                                    var fileReader = new FileReader();
+                                    fileReader.onload = (function() {
+                                        document.getElementById('preview').src = fileReader.result;
+                                    });
+                                    fileReader.readAsDataURL(obj.files[0]);
+                                }
+
+                                $(function() {
+                                    $(document).on('change', '#receipt', function() {
+                                        $('#preview').addClass('img-thumbnail');
+                                    });
+                                });
+                            </script>
                         </div>
+                        <div></div>
 
                         <div class="flex justify-center md:px-20 xl:px-20">
                             <label class="block mt-12">
